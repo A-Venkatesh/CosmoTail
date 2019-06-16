455c8f2f8957d8554c3ff6fd3ab2d36e1016032c
A-Venkatesh <venkatesha@live.in>
2019-06-16 17:04:41 +0530

    Working client and server side

--- CosmoTail/src/app/app.module.ts
+++ CosmoTail/src/app/app.module.ts
@@ -1,20 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import{HttpClientModule} from '@angular/common/http';
+import{FormsModule} from'@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { UserComponent } from './user/user.component';
+import { UserService } from './user.service';
+import { from } from 'rxjs';
 
+UserService
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    HttpClientModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [UserService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ CosmoTail/src/app/user/user.component.ts
@@ -0,0 +1,30 @@
+import{User} from './../user';
+
+
+import { Component, OnInit } from '@angular/core';
+import {UserService} from '../user.service';
+
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.css']
+})
+export class UserComponent implements OnInit {
+public user= new User();
+  constructor(private _userService:UserService) { }
+
+  ngOnInit() {
+  }
+  createUser(){
+    this._userService.createUser(this.user).subscribe(
+      data=>{
+        console.log(data);
+      },
+      error=>{
+        console.log(error);
+        
+      }
+    )
+  }
+}
